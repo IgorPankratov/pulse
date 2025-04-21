c9922529cc71f91385798b59b01e56ab647a124f
IgorP <ipankrat85@gmail.com>
2025-04-21 21:56:16 +0300

    The setup for sending emails from the site is completed

--- src/js/script.js
+++ src/js/script.js
@@ -122,7 +122,30 @@ validateForms('#consultation-form');
 validateForms('#consultation form');
 validateForms('#order form');
 
+// Добавили маску ввода телефона
 $('input[name=phone]').mask("+7(999) 999-99-99");
+
+$('form').submit(function(e) {
+  e.preventDefault();
+
+  if (!$(this).valid()) {
+    return;
+  }
+
+  $.ajax({
+    type: "POST",
+    url: "mailer/smart.php",
+    data: $(this).serialize()
+  }).done(function(){
+    $(this).find('input').val("");
+    $('#consultation, #order').fadeOut();
+    $('.overlay, #thanks').fadeIn('slow');
+
+
+    $('form').trigger('reset');
+  });
+  return false;
+});
 
 });
 
